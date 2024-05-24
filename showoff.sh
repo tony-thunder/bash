d4c09ff704acbf1179490680e18fdc41dee63232
tony-thunder <signup@brasscat.tech>
2024-05-23 21:26:19 -0400

    Update showoff.sh
    Corrected issue with docker.service running.

--- showoff.sh
+++ showoff.sh
@@ -1,5 +1,8 @@
 dnf upgrade -y
 dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 dnf install docker-ce docker-ce-cli containerd.io -y
+systemctl enable docker.service
+systemctl start docker.service
 docker pull grafana/grafana
 docker run -d --name=grafana -p 3000:3000 grafana/grafana
+ipconfig
